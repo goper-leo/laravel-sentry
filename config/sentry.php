7186e8066ed192d56e55ba5a9def77240bab0a74
goper <goper@eetechmedia.com>
2017-10-05 13:12:06 +0800

    Setup sentry libraries.

---
+++ config/sentry.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sentry settings
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'table_name' => 'project_views',
+
+    'viewer_details' => [
+        'ip_address',
+        'country',
+        'zipCode',
+        'cityName',
+        'regionName',
+        'latitude',
+        'longitude',
+        'device',
+        'browser',
+        'languages',
+        'os',
+
+    ],
+
+];
